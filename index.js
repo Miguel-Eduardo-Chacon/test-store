73ac93e0318c1915d40d604dc5a215f27efeced0
Miguel-Eduardo-Chacon <miguel.chacon@samishop.pe>
2023-10-19 01:23:21 -0500

    Update renders ejs

--- index.js
+++ index.js
@@ -8,22 +8,19 @@ config();
 
 const app = express();
 const port = process.env.PORT;
-// const __dirname = dirname(fileURLToPath(import.meta.url))
+const __dirname = dirname(fileURLToPath(import.meta.url))
 
-// app.set('views', join(__dirname, 'views'))
-// app.set('view engine', 'ejs');
+app.set('views', join(__dirname, 'views'))
+app.set('view engine', 'ejs');
 
-// console.log(join(__dirname, 'views'))
+console.log(join(__dirname, 'views'))
 
-// app.use(indexRoutes)
-// app.use(express.static(join(__dirname, 'public')))
-// app.use((request, response, next) => {
-//   response.status(404).render('404-page');
-// })
-
-app.use((req, res) => {
-  res.json({message: 'Tamos ready!'})
+app.use(indexRoutes)
+app.use(express.static(join(__dirname, 'public')))
+app.use((request, response, next) => {
+  response.status(404).render('404-page');
 })
+
 app.listen(port);
 
 console.log(`Local Server: ${port}`);
